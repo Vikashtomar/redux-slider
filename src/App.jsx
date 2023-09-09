a49c63961d48cbd4afca73ef46aeaba024da75db
vikash <vikash.tomar.india@gmail.com>
2023-09-09 22:07:17 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
+ 
 import { useEffect } from "react";
-import "./App.css"
+import "./App.css";
 import { useDispatch, useSelector } from "react-redux";
 import { getData, incrementSlider, decrementSlider } from "./slice";
 
@@ -9,44 +10,42 @@ function App() {
     return state.slider;
   });
 
-  
   useEffect(() => {
     dispatch(getData(init.page));
   }, [init.page]);
 
-  if (!init.isLoading) return <p>Loading...</p>;
+  if (!init.isLoading) return <h3>Loading...</h3>;
   else {
     return (
       <>
-      <div className="maindiv">
-
-        <div className="slider">
-          <div className="slider-wrapper">
-            <div className="left">
-              <img src={init.product.image} alt="Product Image" />
-            </div>
-            <div className="right">
-              <h2>{init.product.title}</h2>
-              <p>{init.product.description}</p>
-              <p>{init.product.price}</p>
-            </div>
-          </div>
-          <div className="navigation">
-            <button
-              onClick={() => dispatch(decrementSlider())}
-              disabled={init.page === 1 ? true : false}
-              >
-              Prev
-            </button>
-            <button
-              onClick={() => dispatch(incrementSlider())}
-              disabled={init.page === 19 ? true : false}
-              >
-              Next
-            </button>
-          </div>
-          <h1>hello</h1>
+        <h1>Redux image slider</h1>
+        <div className="container-fluid">
+          <div className="maindiv">
+            <div className="slider">
+              <div className="slider-wrapper">
+                <div className="left">
+                  <img src={init.product.image} alt="Product Image" />
+                </div>
+                <div className="right">
+                  <h2>{init.product.title}</h2>
+                  <p>{init.product.description}</p>
+                  <p>{init.product.price}</p>
+                </div>
               </div>
+              <div className="navigation">
+                <button
+                  onClick={() => dispatch(decrementSlider())}
+                  disabled={init.page === 1 ? true : false}>
+                  Prev
+                </button>
+                <button
+                  onClick={() => dispatch(incrementSlider())}
+                  disabled={init.page === 19 ? true : false}>
+                  Next
+                </button>
+              </div>
+            </div>
+          </div>
         </div>
       </>
     );
